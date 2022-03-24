1b62654b202cc8cffa486390df76c18b356ed4d4
Irangal <irangalgiffary@gmail.com>
2022-03-24 22:10:53 +0700

    pahlawan sampah

---
+++ pahlawan sampah/Assets/script/script tutorial/deteksisampah.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI; 
+
+public class deteksisampah : MonoBehaviour {
+
+	public string nameTag; 
+	public AudioClip audioBenar; 
+	private AudioSource MediaPlayerBenar; 
+	public Text textScore; 
+	// Use this for initialization 
+	void Start() 
+	{ 
+		MediaPlayerBenar = gameObject.AddComponent<AudioSource>();
+		MediaPlayerBenar.clip =audioBenar;  
+
+
+	} 
+	void OnTriggerEnter(Collider collision) 
+	{ 
+		if (collision.tag.Equals (nameTag)) { 
+			Data.score += 1; 
+			textScore.text = Data.score.ToString(); 
+			Destroy (collision.gameObject); 
+			MediaPlayerBenar.Play (); 
+		}
+		}
+	}
+
