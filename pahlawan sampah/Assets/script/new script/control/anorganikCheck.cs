e2f5bbf0aebd4223366c4e9d6193a1645af05f8d
ranggaLP <ranggaghtdghtd@gmail.com>
2022-04-05 09:44:34 +0700

    ngantuk

--- pahlawan sampah/Assets/script/new script/control/anorganikCheck.cs
+++ pahlawan sampah/Assets/script/new script/control/anorganikCheck.cs
@@ -4,9 +4,21 @@ using UnityEngine;
 
 public class anorganikCheck : MonoBehaviour
 {
-    private void OnCollisionEnter2D(Collision2D collision)
+    // Start is called before the first frame update
+    void Start()
     {
-        if (collision.gameObject.CompareTag("NonOrganik"))
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag.Equals("NonOraganik"))
         {
             Destroy(this.gameObject);
         }
