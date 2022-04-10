1fa0d4e82a9523a5efa1b13a7e36694e1e73188f
Irangal <irangalgiffary@gmail.com>
2022-04-10 16:44:11 +0700

    baruu

---
+++ pahlawan sampah/Assets/script/Gameplay4/bg.cs
@@ -0,0 +1,90 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine .UI; 
+
+public class bg : MonoBehaviour {
+	public string nameTag;
+	Rigidbody2D rigid;
+	public Text textScore; 
+	public GameObject HasilJadihiasan;
+	public GameObject HasilJadipot;
+	public GameObject HasilJaditisu;
+	public GameObject pot;
+	public GameObject hiasan;
+	public GameObject tisu;
+	int count ; 
+
+
+	// Use this for initialization
+	void Start () {
+		HasilJadihiasan.SetActive (false);
+		HasilJadipot.SetActive (false);
+		HasilJaditisu.SetActive (false);
+
+
+
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Data.pot == pot && count == 2) 
+		{
+			potjadi ();
+
+		}
+		if (Data.hiasan  == hiasan  && count == 3) 
+		{
+			hiasanJadi  ();
+
+		}
+		if (Data.tisu   == tisu   && count == 4) 
+		{
+			tisuJadi  ();
+
+		}
+		rigid = GetComponent <Rigidbody2D > ();
+	}
+	void OnTriggerEnter2D (Collider2D collision)
+	{ 
+
+		if (collision.tag.Equals (nameTag)) {
+			DestroyObject (collision.gameObject);
+			count++; 
+		}
+			
+	}
+
+	public void HasilJadipotClicked ()
+	{
+
+		pot.SetActive (true);
+	}
+	public void HasilJadihiasanClicked ()
+	{
+		hiasan.SetActive (true);
+
+	}
+	public void hasilJaditisuClicked ()
+	{
+		tisu.SetActive(true);
+	}
+		
+	void potjadi ()
+	{
+		count = 0;
+		HasilJadipot.SetActive (true);
+	}
+	void hiasanJadi ()
+	{
+		count = 0; 
+		HasilJadihiasan.SetActive (true);
+	}
+		void tisuJadi ()
+		{
+			count = 0; 
+			HasilJaditisu.SetActive (true);
+
+}
+}
---
+++ pahlawan sampah/Assets/script/Gameplay4/panel.cs
@@ -0,0 +1,128 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement ;
+
+public class panel: MonoBehaviour {
+	public GameObject awalmenu;
+	public GameObject milihmenupot;
+	public GameObject milihmenuhiasan;
+	public GameObject milihmenutisu;
+	public GameObject kembali;
+	public GameObject hasilpot;
+	public GameObject hasilhiasan;
+	public GameObject hasiltisu;
+	public GameObject HasilJadipot;
+	public GameObject HasilJadihiasan;
+	public GameObject HasilJaditisu;
+
+
+
+
+	// Use this for initialization
+	void Start () {
+		awalmenu.SetActive (true);
+		milihmenupot.SetActive (false);
+		milihmenuhiasan.SetActive (false);
+		milihmenutisu.SetActive (false);
+		kembali.SetActive (true);
+		HasilJadipot.SetActive (false);
+		HasilJadihiasan.SetActive (false);
+		HasilJaditisu.SetActive (false);
+		hasilpot.SetActive (false);
+		hasilhiasan.SetActive (false);
+		hasiltisu.SetActive (false);
+	
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+	}
+	public void HasilJadipotClicked()
+	{
+		awalmenu.SetActive (false);
+		milihmenupot.SetActive (false);
+		milihmenuhiasan.SetActive (false);
+		milihmenutisu.SetActive (false);
+		HasilJadipot.SetActive (true);
+		HasilJadihiasan.SetActive (false);
+		HasilJaditisu.SetActive (false);
+
+	}
+	public void HasilJadihiasanClicked()
+	{
+		awalmenu.SetActive (false);
+		milihmenupot.SetActive (false);
+		milihmenuhiasan.SetActive (false);
+		milihmenutisu.SetActive (false);
+		HasilJadipot.SetActive (false);
+		HasilJadihiasan.SetActive (true);
+		HasilJaditisu.SetActive (false);
+
+	}
+	public void HasilJaditisuClicked()
+	{
+		awalmenu.SetActive (false);
+		milihmenupot.SetActive (false);
+		milihmenuhiasan.SetActive (false);
+		milihmenutisu.SetActive (false);
+		HasilJadipot.SetActive (false);
+		HasilJadihiasan.SetActive (false);
+		HasilJaditisu.SetActive (true);
+
+	}
+
+		
+	public void kembaliClicked()
+	{
+		awalmenu.SetActive (true);
+		milihmenupot.SetActive (false);
+		milihmenuhiasan.SetActive (false);
+		milihmenutisu.SetActive (false);
+		Data.pot = false;
+
+
+	}
+
+	public void potClicked()
+	{
+		awalmenu.SetActive (false);
+		milihmenupot.SetActive (true);
+		milihmenuhiasan.SetActive (false);
+		milihmenutisu.SetActive (false);
+		kembali.SetActive (true);
+		Data.pot = true;
+	}
+	public void tisuClicked()
+	{
+
+		awalmenu.SetActive (false);
+		milihmenupot.SetActive (false);
+		milihmenuhiasan.SetActive (false);
+		milihmenutisu.SetActive (true);
+		kembali.SetActive (true);
+		Data.tisu = true;
+	}
+	public void hiasanClicked
+	()
+	{
+		awalmenu.SetActive (false);
+		milihmenupot.SetActive (false);
+		milihmenuhiasan.SetActive (true);
+		milihmenutisu.SetActive (false);
+		kembali.SetActive (true);
+		Data.hiasan = true; 
+	}
+	public void Quit_Clicked()
+	{
+		Application.Quit ();
+
+	}
+
+
+
+
+
+
+	}
